94495695ab2a73c19b4c3265c5cb24f7aede11af
victorizbitskiy <victor.izbitskiy@yandex.ru>
2021-11-27 21:25:22 +0300

    added footer

--- index.js
+++ index.js
@@ -5,5 +5,16 @@ const modal = $.modal({
   <p>lorem ipsum dolor sit.</p>
   <p>lorem ipsum dolor sit.</p>
   `,
-  width: '400px'
+  width: '400px',
+  footerButtons: [
+    {text: 'Ok', type: 'primary', handler() {
+      console.log('Primary btn clicked')
+      modal.close()
+    }},
+    {text: 'Cancel', type: 'danger', handler() {
+      console.log('Danger btn clicked')
+      modal.close()
+    }},
+
+  ]
 })
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,3 +1,30 @@
+Element.prototype.appendAfter = function (element) {
+  element.parentNode.insertBefore(this, element.nextSibling)
+}
+
+function noop() { }
+
+function _createModalFooter(buttons = []) {
+  if (buttons.length === 0) {
+    return document.createElement('div')
+  }
+
+  const wrap = document.createElement('div')
+  wrap.classList.add('modal-footer')
+
+  buttons.forEach(btn => {
+    const $btn = document.createElement('button')
+    $btn.textContent = btn.text
+    $btn.classList.add('btn')
+    $btn.classList.add(`btn-${btn.type || 'secondary'}`)
+    $btn.onclick = btn.handler || noop
+
+    wrap.appendChild($btn)
+  })
+
+  return wrap
+}
+
 function _createModal(options) {
   const DEFAULT_WIDTH = '600px'
   const modal = document.createElement('div')
@@ -12,13 +39,11 @@ function _createModal(options) {
         <div class="modal-body" data-content>
           ${options.content || ''}
         </div>
-        <div class="modal-footer">
-          <button>Ok</button>
-          <button>Cancel</button>
-        </div>
       </div>
     </div>
   `)
+  const footer = _createModalFooter(options.footerButtons)
+  footer.appendAfter(modal.querySelector('[data-content]'))
   document.body.appendChild(modal)
   return modal
 }
