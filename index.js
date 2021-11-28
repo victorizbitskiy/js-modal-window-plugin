9c551444389854e95b7e047bc07898cdd0d8f752
victorizbitskiy <victor.izbitskiy@yandex.ru>
2021-11-28 11:03:45 +0300

    added bug fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fruits = [
-{id: 1, title: 'Яблоки', price: 20, img: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348'}
-{id: 1, title: 'Апельсины', price: 20, img: 'https://www.prismamarket.ru/upload/iblock/9c0/2000650200001_Apelsiny.PNG.png'}
+{id: 1, title: 'Яблоки', price: 20, img: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348'},
+{id: 1, title: 'Апельсины', price: 20, img: 'https://www.prismamarket.ru/upload/iblock/9c0/2000650200001_Apelsiny.PNG.png'},
 {id: 1, title: 'Манго', price: 20, img: 'https://travel.miruvashihnog.ru/wp-content/uploads/2017/03/mango-1-300x199.jpg'}
 ]
 
